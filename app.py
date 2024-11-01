2c773b4c5927a261a4d2739c652c0628ee2a6bcc
jacoblebo <jumpimaus@gmail.com>
2024-11-01 15:59:15 -0400

    Fixing my dumb mistake- didn't put a db query in a request

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
-from models import db, User, Attendance, Class, Enrollment, Session
+from flask_sqlalchemy import SQLAlchemy
 import bcrypt
 import os
 import re # we love regex
@@ -10,6 +10,47 @@ app.secret_key = os.getenv('SECRET_KEY')
 # Configure the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(50), nullable=False)
+    lastName = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.Integer, nullable=False)
+
+class Attendance(db.Model):
+    __tablename__ = 'Attendance'
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.Integer, db.ForeignKey('Sessions.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('Users.id'))
+    status = db.Column(db.Integer)
+
+class Class(db.Model):
+    __tablename__ = 'Classes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    professor_id = db.Column(db.Integer, nullable=False)
+    latitude = db.Column(db.Integer, nullable=False)
+    longitude = db.Column(db.Integer, nullable=False)
+    no_location = db.Column(db.Integer, nullable=False)
+
+class Enrollment(db.Model):
+    __tablename__ = 'Enrollments'
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey('Classes.id'), nullable=False)
+
+class Session(db.Model):
+    __tablename__ = 'Sessions'
+    id = db.Column(db.Integer, primary_key=True)
+    class_id = db.Column(db.Integer, db.ForeignKey('Classes.id'))
+    session_start_date = db.Column(db.String(50))
+    session_end_date = db.Column(db.String(50))
+    bypass_code = db.Column(db.String(50))
 
 db.init_app(app)
 
@@ -28,6 +69,7 @@ def login():
         password = request.form['password'].encode('utf-8')
 
         # Authenticate user
+        users = User.query.all()
         user = users.get(email)
         if user and bcrypt.checkpw(password, user['password']):
             # Set user info in session
